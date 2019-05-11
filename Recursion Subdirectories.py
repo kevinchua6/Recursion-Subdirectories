9aa692984ebba04f57076e6b4073242df9cd128a
yourally2 <yourally2@users.noreply.github.com>
2019-05-11 09:48:27 +0800

    Made the code cleaner

--- Recursion Subdirectories.py
+++ Recursion Subdirectories.py
@@ -1,22 +1,7 @@
 import os, shutil
 '''Using recursion to iterate through all subdirectories and transfer it to the main directory.'''
+
 srcpath = r"D:\Anime\speed power test"
-os.chdir(srcpath)
-
-
-cwd= r'D:\Anime\speed power test'
-dwd= r'D:\Anime\speed power test\Dororo'
-#def transfer(srcpath, destpath):
-#    
-#    file_dir = os.listdir(destpath)
-#    print('file dir',file_dir)
-#    for fileName in file_dir:
-#        print(fileName)
-#        if os.path.isdir(fileName):
-#            transfer(srcpath, fileName)
-#        elif fileName.endswith('.mkv') or fileName.endswith('.mp4'):
-#            print ("Moving "+ fileName)
-#            shutil.move(os.path.join(srcpath,fileName), srcpath)
 
 #ensure that the srcpath always stays constant, so its passed on to the next function varibales
 def transfer(srcpath, cwd):
@@ -27,7 +12,5 @@ def transfer(srcpath, cwd):
         elif (fileName.endswith('.mkv') or fileName.endswith('.mp4')) and cwd != srcpath:
             print ("Moving "+ fileName)
             shutil.move(os.path.join(cwd,fileName), srcpath)
-
-
 
 transfer(srcpath, srcpath)
